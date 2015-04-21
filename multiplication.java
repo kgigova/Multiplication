37c3d44e1328147c9c4ccdc7854b6af63f1f11dc
Kristiana Gigova <kristiana.gigova@gmail.com>
2015-04-21 22:22:26 +0300

    Update multiplication.java
    Fixed some issues.

--- multiplication.java
+++ multiplication.java
@@ -9,20 +9,18 @@ public class Multiplication {
 
 	/** * @param args the command line arguments */
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System. in );
 		System.out.println("N:");
 		int N = scan.nextInt();
-		if (N < 0 || N > 3200000) {
+		if (N > 0 && N < 3200000) {
+			String redica = "";
+			int tekushtoChislo = 1;
+			while (redica.length() < N) {
+				redica += String.format("%d", (tekushtoChislo * tekushtoChislo));
+				tekushtoChislo ++;
+			}
+			System.out.println(redica.substring(N - 1, N));
+		} else {
 			System.out.println("N trqbva da e mejdu 0 i 3200000");
-			return;
 		}
-		string redica = "";
-		int tekushtoChislo = 1;
-		while (redica.Length < n) {
-			redica += String.format("%d", (tekushtoChislo * tekushtoChislo));
-			tekushtoChislo++
-		}
-		System.out.println(redica.Substring(N - 1, N));
 	}
-
 }
